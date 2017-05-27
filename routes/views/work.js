7e185794cf435c6af593ed77039547fdf1934e09
Bruno Cabezas <bruno@Brunos-MacBook-Pro.local>
2017-05-27 14:41:09 -0400

    same as prev commit

--- routes/views/work.js
+++ routes/views/work.js
@@ -1,9 +1,13 @@
 var keystone = require('keystone');
-var Enquiry = keystone.list('Enquiry');
+// var keystone = require('masonry-layout');
 
 exports = module.exports = function (req, res) {
 
 	var view = new keystone.View(req, res);
 
+	// Load the galleries by sortOrder
+	view.query('works', keystone.list('Work').model.find().sort('publishedDate'));
+
+	// Render the view
 	view.render('work');
 };

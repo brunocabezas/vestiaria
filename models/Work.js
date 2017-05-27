f73f994566aaf68b7cfcf62ff223c6c87df48133
Bruno Cabezas <bruno@Brunos-MacBook-Pro.local>
2017-05-27 14:39:48 -0400

    added jeet as library, removed bootstrap,added new Work model, modified work view and style

---
+++ models/Work.js
@@ -0,0 +1,23 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Work Model
+ * =============
+ */
+
+var Work = new keystone.List('Work', {
+	autokey: { from: 'name', path: 'slug', unique: true },
+});
+
+Work.add({
+	name: { type: String, required: true },
+	heroImage: { type: Types.CloudinaryImage },
+	publishedDate: { type: Date, default: Date.now },
+	date  : { type: Date, default: Date.now },
+	abstract : { type: String },
+	gallery: { type: Types.CloudinaryImages },
+	video : { type: String }
+});
+
+Work.register();

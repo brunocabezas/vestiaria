b96177ef2cccee70eacff16f53fbdf5af3736155
Bruno Cabezas <bruno@Brunos-MacBook-Pro.local>
2017-05-27 20:17:20 -0400

    improved layout system, added overlay plugin, now displaying works on overlay

--- routes/views/index.js
+++ routes/views/index.js
@@ -5,6 +5,9 @@ exports = module.exports = function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
+	// Load the galleries by sortOrder
+	view.query('works', keystone.list('Work').model.find().sort('publishedDate'));
+
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
